a78ce2a3036079ace9d48cc00756e87759356fb4
KobinaOtc <kobinaotchereadjoku@gmail.com>
2026-07-06 18:17:31 +0000

    list assignment done.

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -8,6 +8,23 @@ public class Program
         // with any code or ideas you have that do not directly apply to
         // one of your projects.
 
+        // Fixed arrays example.
+        var numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+
+        var altNumbers = new [] {1, 2, 3};
+
+        // Lists example.
+        var dynamicNumbers = new List<int>();
+        dynamicNumbers.Add(1);
+        dynamicNumbers.Add(2);
+        dynamicNumbers.Add(3);
+
+        var altDynamicNumbers = new List<int> {1, 2, 3};
+
+
         Console.WriteLine("Hello Sandbox World!");
     }
 }
